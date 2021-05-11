153ee93f0ca0f3baf7ea898fb90b0b9d1a8c11ec
HaiseCoffee <44026596+HaiseCoffee@users.noreply.github.com>
2021-05-10 21:52:26 -0300

    Revert "dropdown juego"

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
       <h1>Juego</h1>
-      <div>
-        <b-dropdown text="Button text via Prop">
-          <b-dropdown-item href="#">An item</b-dropdown-item>
-          <b-dropdown-item href="#">Another item</b-dropdown-item>
-        </b-dropdown>
-      </div>
   </div>
 </template>
 
